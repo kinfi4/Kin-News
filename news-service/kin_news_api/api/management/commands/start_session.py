333b7f60ffc56666bc4c8369831d0d742154df70
kinfi4 <ilyamakarov223@gmail.com>
2022-10-23 22:49:34 +0300

    Removed unused Management script

--- news-service/kin_news_api/api/management/commands/start_session.py
+++
@@ -1,9 +0,0 @@
-from django.core.management import BaseCommand
-
-from api.views import container
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        print('Starting the session...')
-        container.clients.telegram_client().start_session()
