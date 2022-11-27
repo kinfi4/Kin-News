fcc32041711c40977fa0dab6d63cc6b6cbbf05fb
kinfi4 <ilyamakarov223@gmail.com>
2022-11-27 21:07:35 +0200

    Processing status support on frontend

--- kin-news-frontend/kin-frontend/src/components/body/stats/ReportVisualization/ProcessingReport.js
+++ kin-news-frontend/kin-frontend/src/components/body/stats/ReportVisualization/ProcessingReport.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import processingIcon from "../../../../images/processing-icon.png";
 
 const ProcessingReport = () => {
     return (
-        <div>
-            PROCESSING
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "40px",
+                alignItems: "center",
+                justifyContent: "center",
+                color: "#edf2fc",
+                fontSize: "25px",
+            }}
+        >
+            <img src={processingIcon} alt="Processing Icon" width={"250px"} />
+            <h3>Your report is being processing!</h3>
         </div>
     );
 };
